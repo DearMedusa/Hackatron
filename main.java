ccbda8f49369f0208fa01c3ca1e9cd8b6e3b167f
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-12 16:30:56 +0100

    Update main.java

--- main.java
+++ main.java
@@ -12,32 +12,32 @@ public class main {
 		String[] h1 = { "" };
 		Server hub = new Server("Hub server", h1);
 
-		String[] s1 = { "NePasOuvrirEnPublic.png", "NotesDuDS.xl", "MoiQuiDanse.mp4","ChienQuiDort.gif"};
+		String[] s1 = { "NePasOuvrirEnPublic.png", "NotesDuDS.xl", "MoiQuiDanse.mp4", "ChienQuiDort.gif" };
 		Server serv1 = new Server("serveur 1", s1);
-		
-		String[] s2 = { "Sudoku.java", "HistoiresDeJojo.html", "AvisDesHaters.trash"};
+
+		String[] s2 = { "Sudoku.java", "HistoiresDeJojo.html", "AvisDesHaters.trash" };
 		Server serv2 = new Server("serveur 2", s2);
-		
+
 		String[] s3 = { "KaamelottIntegrale.zip", "ImagesDeCanard.png", "RickAstley.mp3", "Fortnite.exe" };
 		Server serv3 = new Server("serveur 3", s3);
-		
-		String[] s4 = { "a","a","a","a","a","a","a"};
+
+		String[] s4 = { "a", "a", "a", "a", "a", "a", "a" };
 		Server serv4 = new Server("serveur 4", s4);
-		
+
 		// CONNEXION DES SERVEURS ENTRE EUX
-		
-		Server[] voisinsHub = {serv1,serv2,serv3};
+
+		Server[] voisinsHub = { serv1, serv2, serv3, serv4 };
 		hub.setVoisins(voisinsHub);
-		Server[] voisinsserv1 = {hub,serv2,serv3};
+		Server[] voisinsserv1 = { hub, serv2, serv3, serv4 };
 		serv1.setVoisins(voisinsserv1);
-		Server[] voisinsserv2 = {hub,serv1,serv3};
+		Server[] voisinsserv2 = { hub, serv1, serv3, serv4 };
 		serv2.setVoisins(voisinsserv2);
-		Server[] voisinsserv3 = {hub,serv1,serv2};
+		Server[] voisinsserv3 = { hub, serv1, serv2, serv4 };
 		serv3.setVoisins(voisinsserv3);
 
 		// DEFINITION DU SERVEUR DE DEPART
 		p.setCurrentServ(hub);
-		
+
 		// INTRO
 		System.out.println("  _    _          _____ _  __       _______ _____   ____  _   _ \r\n"
 				+ " | |  | |   /\\   / ____| |/ /    /\\|__   __|  __ \\ / __ \\| \\ | |\r\n"
@@ -45,7 +45,7 @@ public class main {
 				+ " |  __  | / /\\ \\| |    |  <    / /\\ \\ | |  |  _  /| |  | | . ` |\r\n"
 				+ " | |  | |/ ____ \\ |____| . \\  / ____ \\| |  | | \\ \\| |__| | |\\  |\r\n"
 				+ " |_|  |_/_/    \\_\\_____|_|\\_\\/_/    \\_\\_|  |_|  \\_\\\\____/|_| \\_|");
-		System.out.println("Bienvenue sur Hackatron, le meilleur logiciel de piratage connu");
+		System.out.println("Bienvenue sur Hackatron 3000, le meilleur logiciel de piratage connu");
 		System.out.println("***********************************************************************");
 		System.out.println(
 				"Votre objectif est de telecharger Sudoku.java, vous savez que ce fichier est situe quelquepart sur le reseau ");
@@ -57,9 +57,9 @@ public class main {
 		p.setPseudo(sc.nextLine());
 
 		System.out.println("Votre nom d'utilisateur a ete defini a: " + p.getPseudo());
-		
+
 		// GAME LOOP
-		
+
 		while (running) {
 			c.input(p, sc);
 			// interactions
@@ -74,7 +74,7 @@ public class main {
 				Command.help();
 				break;
 			case "backdoor":
-				Command.backdoor();
+				Command.backdoor(p);
 				break;
 			case "ifconfig":
 				Command.ifconfig(p);
