03a3802e22ceff5780544b11d6e44f252241251f
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-13 11:52:38 +0100

    Update map.java

--- map.java
+++ map.java
@@ -1,14 +1,16 @@
 import java.awt.Color;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
-public class Map extends JPanel{
-	
+public class Map extends JPanel {
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.black);
-		g.drawOval(50, 50, 50, 50);
+		g.setColor(Color.green);
+
+		//for (int i = 0; i < tableaudeServeurs.length(); i++) {
+		//	g.drawOval(i * 50, 50, 50, 50);
+		//}
 	}
 
 }
