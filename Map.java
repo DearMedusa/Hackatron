39505c94b8ffa8abce41093348f7b84ab48624f2
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-23 20:06:23 +0100

    Ajout du système de visualisation des serveurs [TEMPORAIRE]

--- Map.java
+++ Map.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.util.Random;
 
 public class Map extends JPanel {
 	
@@ -9,12 +10,32 @@ public class Map extends JPanel {
 	*/
 
 	public void paintComponent(Graphics g) {
+		Random random = new Random();
 		super.paintComponent(g);
-		g.setColor(Color.green);
-
-		//for (int i = 0; i < tableaudeServeurs.length(); i++) {
-		//	g.drawOval(i * 50, 50, 50, 50);
-		//}
+		int h = getHeight()-50;
+		int w = getWidth()-50;
+		
+		//créer un cercle par serveur et affiche son nom
+		for (int i = 0; i < GenerationServeurs.getTabServer().length; i++) {
+			int coordXCercle = random.nextInt(w);
+			int coordYCercle = random.nextInt(h);
+			if (GenerationServeurs.getTabServer()[i].hasAntivirus()) {
+				g.setColor(Color.red);
+			}else if(GenerationServeurs.getTabServer()[i] ==Player.getCurrentServ()){
+				g.setColor(Color.blue);
+			}else {
+				g.setColor(Color.green);
+			}
+			g.drawOval(coordXCercle, coordYCercle, 50,50);
+			g.drawString(GenerationServeurs.getTabServer()[i].getName(),coordXCercle,coordYCercle);
+			
+			//parcours tous les voisins du serveur i
+			for(int j = 0; j < GenerationServeurs.getTabServer()[i].getVoisins().length;j++) {
+				g.drawLine(coordXCercle, coordYCercle, h/2, w/2);
+			}
+		}
+		
+		
 	}
 
 }
