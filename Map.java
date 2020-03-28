8f8febb0b8d47763b83a1728f80d084ed4d6a128
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-28 10:59:45 +0100

    Update Map.java

--- Map.java
+++ Map.java
@@ -6,38 +6,40 @@ import java.util.Random;
 public class Map extends JPanel {
 
 	/*
-	 * Description de la classe
+	 * Classe qui gere l'affichage du reseau apres execution de la commande "map"
 	 */
 
 	public void paintComponent(Graphics g) {
 		Random random = new Random();
 		super.paintComponent(g);
-		int h = getHeight() - 50;
-		int w = getWidth() - 50;
+		int h = getHeight() - 70;
+		int w = getWidth() - 70;
 
 		// créer un cercle par serveur et affiche son nom
 		for (int i = 0; i < GenerationServeurs.getTabServer().length; i++) {
 			
-			int coordXCercle = random.nextInt(w);//coordonnees aleatoire
+			int coordXCercle = random.nextInt(w);//coordonnees aleatoires
 			int coordYCercle = random.nextInt(h);
-			int lastServX = coordXCercle;
-			int lastServY = coordYCercle;
+			int lastServX,currentServX = 0;
+			int lastServY,currentServY = 0;
 			
 			if (GenerationServeurs.getTabServer()[i].hasAntivirus()) {//si le serveur a un antivirus
 				g.setColor(Color.red);//l'afficher en rouge
 				
 			} else if (GenerationServeurs.getTabServer()[i] == Player.getCurrentServ()) {
 				g.setColor(Color.blue);//s'il s'agit du serveur du joueur le mettre en bleu
+				currentServX = coordXCercle;
+				currentServY = coordYCercle;
 				
 			} else if (GenerationServeurs.getTabServer()[i] == Player.getLastServer()) {
 				 lastServX = coordXCercle;
 				 lastServY = coordYCercle;
+				 g.drawLine(currentServX, currentServY, lastServX, lastServY);
 				 
 			} else {
 				g.setColor(Color.green);
 			}
 			g.drawOval(coordXCercle, coordYCercle, 50, 50);
-			g.drawLine(coordXCercle, coordYCercle, lastServX, lastServY);
 			g.drawString(GenerationServeurs.getTabServer()[i].getName(), coordXCercle, coordYCercle);
 			
 		}
