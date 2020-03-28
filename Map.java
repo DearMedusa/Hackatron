082ff5f75bd29c13ecfac8ec35a12c4eac5c1a06
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-28 10:51:34 +0100

    [pas encore fonctionnel] changement de l'affichage en prévision du tracé du réseau

--- Map.java
+++ Map.java
@@ -4,38 +4,44 @@ import javax.swing.JPanel;
 import java.util.Random;
 
 public class Map extends JPanel {
-	
+
 	/*
-	* Description de la classe
-	*/
+	 * Description de la classe
+	 */
 
 	public void paintComponent(Graphics g) {
 		Random random = new Random();
 		super.paintComponent(g);
-		int h = getHeight()-50;
-		int w = getWidth()-50;
-		
-		//créer un cercle par serveur et affiche son nom
+		int h = getHeight() - 50;
+		int w = getWidth() - 50;
+
+		// créer un cercle par serveur et affiche son nom
 		for (int i = 0; i < GenerationServeurs.getTabServer().length; i++) {
-			int coordXCercle = random.nextInt(w-30);
-			int coordYCercle = random.nextInt(h-30);
-			if (GenerationServeurs.getTabServer()[i].hasAntivirus()) {
-				g.setColor(Color.red);
-			}else if(GenerationServeurs.getTabServer()[i] ==Player.getCurrentServ()){
-				g.setColor(Color.blue);
-			}else {
+			
+			int coordXCercle = random.nextInt(w);//coordonnees aleatoire
+			int coordYCercle = random.nextInt(h);
+			int lastServX = coordXCercle;
+			int lastServY = coordYCercle;
+			
+			if (GenerationServeurs.getTabServer()[i].hasAntivirus()) {//si le serveur a un antivirus
+				g.setColor(Color.red);//l'afficher en rouge
+				
+			} else if (GenerationServeurs.getTabServer()[i] == Player.getCurrentServ()) {
+				g.setColor(Color.blue);//s'il s'agit du serveur du joueur le mettre en bleu
+				
+			} else if (GenerationServeurs.getTabServer()[i] == Player.getLastServer()) {
+				 lastServX = coordXCercle;
+				 lastServY = coordYCercle;
+				 
+			} else {
 				g.setColor(Color.green);
 			}
-			g.drawOval(coordXCercle, coordYCercle, 50,50);
-			g.drawString(GenerationServeurs.getTabServer()[i].getName(),coordXCercle,coordYCercle);
+			g.drawOval(coordXCercle, coordYCercle, 50, 50);
+			g.drawLine(coordXCercle, coordYCercle, lastServX, lastServY);
+			g.drawString(GenerationServeurs.getTabServer()[i].getName(), coordXCercle, coordYCercle);
 			
-			//parcours tous les voisins du serveur i
-			for(int j = 0; j < GenerationServeurs.getTabServer()[i].getVoisins().length;j++) {
-				g.drawLine(coordXCercle, coordYCercle, h/2, w/2);
-			}
 		}
-		
-		
+
 	}
 
 }
