85b9226236a4de4e13f18ec0c757546c9e58a7a7
Kotora-source <61540072+Kotora-source@users.noreply.github.com>
2020-04-13 14:51:58 +0200

    optimisation

--- source/Hackatron.java
+++ source/Hackatron.java
@@ -1,11 +1,9 @@
 package source;
-import java.util.*;
 
 public class Hackatron {
 
 	public static void main(String[] args) {
-		//creation de l'instance du jeu
-		Game game = new Game();
+		new Game();
 		
 	}
 }
