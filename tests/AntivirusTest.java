839e95a46cedf98f08dcc5b70f1052ae1bc75d8a
Kotora-source <61540072+Kotora-source@users.noreply.github.com>
2020-04-13 16:32:18 +0200

    Update AntivirusTest.java

--- tests/AntivirusTest.java
+++ tests/AntivirusTest.java
@@ -14,7 +14,7 @@ public class AntivirusTest {
 	@Test
 	public void test_constructeur() {
 		Antivirus test = new Antivirus("test", 1);
-		assertEquals("le nom doit etre test", test.getname(), "test");
+		assertEquals("le nom doit etre test", test.getName(), "test");
 		assertEquals("le lvl doit etre de 1", test.getlvl(), 1);
 		assertEquals("this.Active doit etre true", test.getStatut(), true);
 		test.disable();
